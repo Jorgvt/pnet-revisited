fc6744a63f315a5293ada93e3323e0d618602665
Jorgvt <vila_tronik@hotmail.com>
2026-06-29 16:38:23 +0200

    config

--- Training/IQA/config.py
+++ Training/IQA/config.py
@@ -5,17 +5,12 @@ def get_config():
         "BATCH_SIZE": 64,
         "EPOCHS": 500,
         "SEED": 42,
-        "LEARNING_RATE": 0.003,
-        "INITIAL_LR": 0.01,
-        "PEAK_LR": 0.04,
-        "END_LR": 0.005,
-        "WARMUP_EPOCHS": 15,
+        "LEARNING_RATE": 0.03,
+        # "INITIAL_LR": 0.01,
+        # "PEAK_LR": 0.04,
+        # "END_LR": 0.005,
+        # "WARMUP_EPOCHS": 15,
         # Default freeze patterns for IQA: freeze everything except the final GDN Control layer
-        "FREEZE_PATTERNS": [
-            "GDNGamma_0",
-            "CenterSurroundLogSigmaK_0",
-            "DN_0",
-            "GaborLayerGammaHumanLike__0",
-        ],
+        "FREEZE_PATTERNS": [],
     })
     return config
