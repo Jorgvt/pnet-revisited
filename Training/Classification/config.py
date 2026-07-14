374e833f465afcd9d1a3af35ba132cac4350322a
Jorgvt <vila_tronik@hotmail.com>
2026-07-14 13:23:52 +0200

    updated config

--- Training/Classification/config.py
+++ Training/Classification/config.py
@@ -3,13 +3,13 @@ from ml_collections import ConfigDict
 def get_config():
     config = ConfigDict({
         "BATCH_SIZE": 64,
-        "EPOCHS": 50,
-        "LEARNING_RATE": 1e-3,
+        "EPOCHS": 500,
+        "LEARNING_RATE": 1e-2,
         "SEED": 42,
         "GAP": True, # Global Average Pooling before the classification head
         
         # Freezing configuration: list of path substrings to freeze
         # By default, freeze the entire feature extractor ("perceptnet")
-        "FREEZE_PATTERNS": ["perceptnet"],
+        "FREEZE_PATTERNS": [],
     })
     return config
